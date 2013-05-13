688fb36407cf292d73478039dd29bde836ca79b6
Fen Labalme <fen@openprivacy.org>
2013-05-13 15:47:09 -0400

    better (un)bolding

--- bash_inc/prompt.bash
+++ bash_inc/prompt.bash
@@ -15,16 +15,16 @@ function prompt() {
   if [ "$UID" = "0" ]; then
     local user_color=$bold_red
   else
-    local user_color=$bold_green
+    local user_color=$green
   fi
 
   if [ "$SSH_CLIENT" != "" ]; then
-    local user_ssh="${bold_red}SSH$reset_color "
+    local user_ssh="${red}SSH$reset_color "
   else
     local user_ssh=''
   fi
 
-  PS1="\n$(scm_char) ${user_ssh}[$user_color\u$reset_color@$bold_green\H$reset_color] $yellow\w${reset_color}$git_prompt\n$bold_red$prompt_char$reset_color"
+  PS1="\n$(scm_char) ${user_ssh}[$user_color\u$reset_color@$green\H$reset_color] $yellow\w${reset_color}$git_prompt\n$green$prompt_char$reset_color"
   PS2='> '
   PS4='+ '
 }
@@ -32,8 +32,8 @@ function prompt() {
 
 PROMPT_COMMAND=prompt
 
-SCM_THEME_PROMPT_DIRTY=" $red✗${reset_color}"
-SCM_THEME_PROMPT_CLEAN=" $green✓${reset_color}"
+SCM_THEME_PROMPT_DIRTY=" $bold_red✗${reset_color}"
+SCM_THEME_PROMPT_CLEAN=" $bold_green✓${reset_color}"
 SCM_THEME_PROMPT_PREFIX='|'
 SCM_THEME_PROMPT_SUFFIX=''
 
