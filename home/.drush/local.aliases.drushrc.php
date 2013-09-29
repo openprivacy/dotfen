16fe9d411bca076bda538624d3780b5155620875
Fen Labalme <fen@openprivacy.org>
2013-09-29 17:02:19 -0400

    local alias - need to add more

---
+++ home/.drush/local.aliases.drushrc.php
@@ -0,0 +1,28 @@
+<?php
+$aliases['parent'] = array(
+  'path-aliases' => array(
+    '%dump-dir' => 'nobackup',
+  ),
+  'source-command-specific' => array (
+    'sql-sync' => array (
+      'no-cache' => TRUE,
+      'structure-tables-key' => 'common',
+    ),
+  ),
+  'command-specific' => array (
+    'sql-sync' => array (
+      'no-ordered-dump' => TRUE,
+      'structure-tables-list' => array(
+        'common' => array(
+          'cache', 'cache_filter', 'cache_menu', 'cache_page',
+          'history', 'sessions', 'watchdog',
+        ),
+      ),
+    ),
+  ),
+);
+$aliases['dscadev'] = array(
+  'parent' => '@local.parent',
+  'root'   => "/home/fen/workspace/dscadev/docroot",
+  'uri'    => "http://dscadev.fen.net",
+);
